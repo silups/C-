c30d137311f3f2c71babcee02191b6841ee086cc
silups <159236884+silups@users.noreply.github.com>
2024-03-08 19:41:52 +0900

    백준 1712번 실패 코드

---
+++ 백준 1712.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(void) {
+
+	int  A, B, C;
+	int sum = 0, add = 0, N = 0;
+
+	scanf_s("%d %d %d", &A, &B, &C);
+
+	if (B >= C)
+		printf("-1");
+
+	while (1) {
+
+		sum = A + (B * N);
+		add = C * N;
+		
+		if (sum < add)
+			break;
+
+
+		N++;
+	}
+	
+	printf("%d", N);
+
+}
