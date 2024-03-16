c2d3a7112e266c22e75693fbd67241e628ac0962
silups <159236884+silups@users.noreply.github.com>
2024-03-17 01:22:34 +0900

    열혈 c 14-1

---
+++ 문제 14-1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+
+/*int SquareBV(int);
+void SquareBR(int*);
+void Swap3(int*, int*, int*);
+
+int main(void) {
+
+	int num = 0, i;
+	int num1 = 1, num2 = 2, num3 = 3;
+
+	scanf_s("%d", &num);
+
+
+	i = SquareBV(num);
+	printf("%d %d\n", i, num);
+
+	SquareBR(&num);
+	printf("%d %d\n", i, num);
+
+	printf("%d %d %d\n", num1, num2, num3);
+	Swap3(&num1, &num2, &num3);
+	printf("%d %d %d\n", num1, num2, num3);
+}
+
+int SquareBV(int n){
+	return n * n;
+}
+
+void SquareBR(int * nump) {
+	*nump *= *(nump);   // int n = *nump; *nump = n*n;
+}
+
+void Swap3(int* nump1, int* nump2, int* nump3) {
+
+	int temp = *nump3;
+	*nump3 = *nump2;
+	*nump2 = *nump1;
+	*nump1 = temp;
+}*/
+
